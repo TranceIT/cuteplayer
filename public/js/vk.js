bbb5f1d4742bd9178a63ea737dbcf97ae1ab1f74
Sergeey Hodko <violetdevelopers@gmail.com>
2017-02-09 09:26:09 +0300

    merge fix

---
+++ public/js/vk.js
@@ -0,0 +1,16 @@
+
+VK.init({
+    apiId: 5536088,
+});
+/*
+var groupId = 0;
+
+VK.Api.call('groups.getById', {group_id: 'cube_music'}, function(response){
+	groupId = response.response[0].gid;
+});
+
+VK.Api.call('wall.get', {domain: 'cube_music', count: 5}, function(response){
+	console.log(response);
+});
+*/
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,10 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
-
-        <!-- Fonts -->
-
-        <!-- Styles -->
+        <script src="https://vk.com/js/api/openapi.js" type="text/javascript"></script>
     </head>
     <body>
-        
+        <script src="./js/vk.js" type="text/javascript"></script>
     </body>
 </html>
